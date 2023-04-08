e4e03f303ef3a1be92b0a07fbc7271dc6ec5de48
Workflowbot46 <129351389+Workflowbot46@users.noreply.github.com>
2023-04-08 06:42:11 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -54,8 +54,8 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             f"**Link:** {share_link}",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://telegram.me/JAsuranBots"),
-                  InlineKeyboardButton("Support Group", url="https://telegram.me/AsuranMoviefinder")]]
+                 [InlineKeyboardButton("Bots Channel", url="https://telegram.me/TamilaRockers"),
+                  InlineKeyboardButton("Support Group", url="https://telegram.me/TR_Discussion")]]
             ),
             disable_web_page_preview=True
         )
@@ -86,7 +86,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         await forwarded_msg.reply_text(
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             disable_web_page_preview=True)
-        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(file_er_id)}"
+        share_link = f"https://telegram.me/{Config.BOT_USERNAME}?start=TamilaRockers_{str_to_b64(file_er_id)}"
         await editable.edit(
             f"**Link:** {share_link}",
             reply_markup=InlineKeyboardMarkup(
